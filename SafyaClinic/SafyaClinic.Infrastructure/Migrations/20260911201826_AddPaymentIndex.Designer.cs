31938374370b7969a73a2a2b461e3d3bcc563d98
MostafaSIbrahim <mostafa.safwat@gmail.com>
2026-09-11 23:19:49 +0300

    migration

--- SafyaClinic/SafyaClinic.Infrastructure/Migrations/20260911201826_AddPaymentIndex.Designer.cs
+++ SafyaClinic/SafyaClinic.Infrastructure/Migrations/20260911201826_AddPaymentIndex.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SafyaClinic.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using SafyaClinic.Infrastructure.Data;
 namespace SafyaClinic.Infrastructure.Migrations
 {
     [DbContext(typeof(SafyaDbContext))]
-    partial class SafyaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260911201826_AddPaymentIndex")]
+    partial class AddPaymentIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SafyaClinic/SafyaClinic.Infrastructure/Migrations/20260911201826_AddPaymentIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SafyaClinic.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_Status_PaymentDate",
+                table: "Payments",
+                columns: new[] { "Status", "PaymentDate" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_Status_PaymentDate",
+                table: "Payments");
+        }
+    }
+}
